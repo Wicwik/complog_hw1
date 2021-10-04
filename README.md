815ff89f18c165f6e515303809e2a3d8f38535a5
Robert Belanec <belanecrobert@gmail.com>
2021-10-04 16:04:56 +0200

    more tests, fixes

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ A python3 script that generates a file containing a set of clauses solving the g
 
 ## Input specification:
 vertices - list of integers
+
 edges - list of pairs (tuples) of integers
 
 colors - list of stirngs (numbers)
@@ -19,7 +20,7 @@ output_file - string (filepath)
 You dont have to do steps 1 and 2 if you have the script in you computer already
 
 1. git clone https://github.com/Wicwik/complog_hw1
-2. cd complog_hw1
+2. cd complog_hw1s
 3. python3 main.py
 
-To convert to dimacs, I was using the text2dimacs.py [cript](https://courses.matfyz.sk/downloads/complog-2017/text2dimacs.py).
+To convert to dimacs, I was using the text2dimacs.py [script](https://courses.matfyz.sk/downloads/complog-2017/text2dimacs.py).
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Author: Robert Belanec
-# Choose and uncommnet a sample test or specify vertices (numbers), edges (pairs of vertices) and colors (letters) 
+# Choose and uncomment a sample test or specify vertices (numbers), edges (pairs of vertices) and colors (letters) 
 
 # TEST1
 vertices = [1, 2, 3, 4]
@@ -21,9 +21,29 @@ colors = ['a', 'b', 'c']
 # edges = [(1,2), (2,3), (3,4), (1,4)]
 # colors = ['a', 'b', 'c', 'd']
 
+# TEST5
+# vertices = [1, 2, 3]
+# edges = [(1,2), (2,3), (3,4), (1,4)]
+# colors = ['a', 'b', 'c']
+
+# TEST6
+# vertices = [1, 2]
+# edges = [(1,2)]
+# colors = ['a']
+
+
 output_file = 'output.txt'
 
 f = open(output_file, 'w')
+
+# remove invalid edges
+to_remove = []
+for e in edges:
+	if e[0] not in vertices or e[1] not in vertices:
+		to_remove.append((e[0], e[1]))
+
+for e in to_remove:
+	edges.remove(e)
 
 # representing edges
 to_found = edges.copy();
@@ -46,7 +66,7 @@ for v in vertices:
 		if c == colors[-1]:
 			f.write('color({0},{1})\n'.format(v, c))
 		else:
-			f.write('color({0},{1}) V '.format(v, c))
+			f.write('color({0},{1}) '.format(v, c))
 
 f.write('\n')
 
@@ -57,17 +77,17 @@ for v in vertices:
 		for cc in colors:
 			if cc != c and (cc, c) not in tried and (c, cc) not in tried:
 				tried.append((c,cc))
-				f.write('-color({0},{1}) V -color({0},{2})\n'.format(v, c, cc))
+				f.write('-color({0},{1}) -color({0},{2})\n'.format(v, c, cc))
 
 f.write('\n')
 
 # neighbours cant have the same color
 for e in edges:
 	for c in colors:
-		f.write('-color({0},{1}) V -edge({0},{2}) V - color({2},{1})\n'.format(e[0], c, e[1]))
+		f.write('-color({0},{1}) -edge({0},{2}) -color({2},{1})\n'.format(e[0], c, e[1]))
 	f.write('\n')
 
 f.close()
 
-print('Output has been written to {0}'.format(output_file))
+print('[INFO] Output has been written to {0}'.format(output_file))
 		
